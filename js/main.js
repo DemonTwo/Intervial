6cf53e8e5859ed8203a40d7c8dbf507c82da729a
DemonTwo <76266733+DemonTwo@users.noreply.github.com>
2021-01-28 23:34:20 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ const selectElement = (element) => document.querySelector(element);
 
 selectElement('.menu-icons').addEventListener( 'click',() => {
     selectElement('nav').classList.toggle('active');
+    selectElement('.sub-menu').classList.toggle('show');
 });
